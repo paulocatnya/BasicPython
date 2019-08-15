3ee56d78d9f446f37c2e571530a7ee278885fc1a
PAULO ROBERTO CAETANO RODRIGUES <proberto@br.ibm.com>
2019-08-15 16:25:29 -0300

    first commit python

---
+++ script.py
@@ -0,0 +1,50 @@
+# Comentário
+""" Meu primeiro app """
+# -*- coding: utf-8  -*-
+"""
+
+
+
+
+print("CALCULADORA EM PYTHON\n")
+print("ENTRE COM PRIMEIRO NÚMERO")
+first = int(input())
+print("ENTRE COM SEGUNDO NMERO")
+second = int(input())
+
+result = first + second
+print(result)
+
+msg = "oi"
+
+print(msg + "." * 3)
+
+
+x = 3
+y = 2
+a = x != y & x < 10
+b = x == y | x > 10
+c = x != y & x < 10
+
+print("true " + str(b))
+print("false " + str(b))
+print("false " + str(b))
+
+
+x = 1
+y = 2
+
+if x > y:
+    print(" x e maior")
+else:
+    print("y e maior")
+
+"""
+
+x = 1
+y = 2
+
+if x > y:
+    print(" x e maior")
+else:
+    print("y e maior")
